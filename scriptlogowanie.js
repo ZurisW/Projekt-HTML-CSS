f36f01f609ded04350b6222a603b0106c0255d14
ZurisW <legman@onet.pl>
2022-02-01 12:48:30 +0100

    Update scriptlogowanie.js

--- scriptlogowanie.js
+++ scriptlogowanie.js
@@ -2,7 +2,7 @@ document.getElementById('submit').addEventListener('click', function(e){
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
 
-    if(username != "Norbert" && password != "haslo"){
+    if(username != "Norbert" || password != "haslo"){
         alert("Nieprawidłowy login lub hasło!");
         e.preventDefault();
     }
